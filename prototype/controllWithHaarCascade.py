b9aa331ecd6b1b257b4c40fb9899175dd1e69a67
leonardoCaceres <leolima212@hotmail.com>
2024-11-08 17:57:23 -0300

    modifica parametros

--- prototype/controllWithHaarCascade.py
+++ prototype/controllWithHaarCascade.py
@@ -2,7 +2,7 @@ import cv2
 from controll_start import *
 import math
 
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(1)
 
 detec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
