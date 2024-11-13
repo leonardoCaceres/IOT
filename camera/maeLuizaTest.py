3ca21bab3eae6b8622cdf962e16aca5fd0cd486e
Leonardo Lima <llima@solidarenovaveis.com.br>
2024-11-13 14:34:50 -0300

    remove corte de frame

--- camera/maeLuizaTest.py
+++ camera/maeLuizaTest.py
@@ -4,10 +4,10 @@ import cv2
 cam = cv2.VideoCapture(0)
 
 # Carregar o classificador Haar para detecção de rosto
-detec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+detec = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 # Definir o nome da janela e permitir que seja redimensionada
-window_name = 'Camera'
+window_name = "Camera"
 cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 
 # Definir a largura e altura da janela para cobrir uma parte da tela
@@ -19,32 +19,28 @@ odd = True
 
 while True:
     ret, frame = cam.read()
-    
+
     if not ret:
         print("Não foi possível capturar a imagem da câmera.")
         break
 
-    if odd:
-        # Converter para escala de cinza para a detecção
-        cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Converter para escala de cinza para a detecção
+    cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Detectar rostos na imagem
-        faces = detec.detectMultiScale(cinza, 1.3, 3)
+    # Detectar rostos na imagem
+    faces = detec.detectMultiScale(cinza, 1.3, 3)
 
-        # Desenhar retângulos ao redor dos rostos detectados
-        for (x, y, larg, alt) in faces:
-            frame = cv2.rectangle(frame, (x, y), (x + larg, y + alt), (0, 255, 0), 3)
+    # Desenhar retângulos ao redor dos rostos detectados
+    for x, y, larg, alt in faces:
+        frame = cv2.rectangle(frame, (x, y), (x + larg, y + alt), (0, 255, 0), 3)
 
-        # Mostrar o frame
-        cv2.imshow(window_name, frame)
-        
-        # Pressione 'q' para sair do loop
-        if cv2.waitKey(1) == ord('q'):
-            break
+    # Mostrar o frame
+    cv2.imshow(window_name, frame)
 
-    odd = not odd
+    # Pressione 'q' para sair do loop
+    if cv2.waitKey(1) == ord("q"):
+        break
 
 # Liberar a captura e fechar as janelas
 cam.release()
 cv2.destroyAllWindows()
-
